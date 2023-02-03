602628afbd3d1157bc515f4bc8a5e82a96ec1938
Takafumi Iju <ijufumi@gmail.com>
2023-02-04 08:27:37 +0900

    Add version command

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/ijufumi/google-vision-sample/pkg/container"
+	"github.com/ijufumi/google-vision-sample/pkg/http/router"
+)
 
+func main() {
+	c := container.NewContainer()
+	r := router.NewRouter(c)
+	err := r.Run()
+	if err != nil {
+		panic(err)
+	}
 }
