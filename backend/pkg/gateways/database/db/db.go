b0bfb983dee3fcd50d3649f9c26ca93b8a24d7ce
Takafumi Iju <ijufumi@gmail.com>
2023-02-03 09:10:31 +0900

    [WIP] Add automigration

--- backend/pkg/gateways/database/db/db.go
+++ backend/pkg/gateways/database/db/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ijufumi/google-vision-sample/pkg/configs"
+	"github.com/ijufumi/google-vision-sample/pkg/gateways/database/entities"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -14,10 +15,17 @@ func NewDB(config *configs.Config) *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
+
+	migrate(db)
+
 	return db
 }
 
 func DsnString(config *configs.Config) string {
 	dbConfig := config.DB
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate(&entities.ExtractionResults{})
 }
