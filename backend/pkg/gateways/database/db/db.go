43b2eb7fc9c455484f2a2f3339e8a6ed60ec6fa7
Takafumi Iju <ijufumi@gmail.com>
2023-02-04 07:54:57 +0900

    Delete auto migration

--- backend/pkg/gateways/database/db/db.go
+++ backend/pkg/gateways/database/db/db.go
@@ -4,19 +4,16 @@ import (
 	"fmt"
 
 	"github.com/ijufumi/google-vision-sample/pkg/configs"
-	"github.com/ijufumi/google-vision-sample/pkg/gateways/database/entities"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDB(config *configs.Config) *gorm.DB {
 	dsn := DsnString(config)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-
-	migrate(db)
 
 	return db
 }
@@ -24,8 +21,4 @@ func NewDB(config *configs.Config) *gorm.DB {
 func DsnString(config *configs.Config) string {
 	dbConfig := config.DB
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
-}
-
-func migrate(db *gorm.DB) {
-	_ = db.AutoMigrate(&entities.ExtractionResults{})
 }
