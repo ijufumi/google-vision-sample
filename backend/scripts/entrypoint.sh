8ea89e7fe6b883d50c1574e9faef6477b640f854
Takafumi Iju <ijufumi@gmail.com>
2023-02-04 09:48:51 +0900

    Add entrypoint script

---
+++ backend/scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare exit
+exit=0
+while [ $exit -eq 0 ]
+do
+  echo "Waiting 10 seconds...."
+  sleep 10s
+  /app/db db version
+  if [ $? -eq 0 ]; then
+    exit=1
+  else
+    echo "App didn't connect to database."
+  fi
+done
+
+/app/db db up
+
+/app/app
