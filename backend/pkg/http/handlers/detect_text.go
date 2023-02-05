fee5d02867a080924055195d60c95e62e63f592a
Takafumi Iju <ijufumi@gmail.com>
2023-02-06 06:50:45 +0900

    [WIP] Add detect text process

--- backend/pkg/http/handlers/detect_text.go
+++ backend/pkg/http/handlers/detect_text.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ijufumi/google-vision-sample/pkg/services"
+	"github.com/ijufumi/google-vision-sample/pkg/utils"
+	"net/http"
+	"os"
 )
 
 type DetectTextHandler interface {
@@ -10,20 +13,44 @@ type DetectTextHandler interface {
 	Post(ctx *gin.Context)
 }
 
-func NewDetectTextHandler(service *services.DetectTextService) DetectTextHandler {
+func NewDetectTextHandler(service services.DetectTextService) DetectTextHandler {
 	return &detectTextHandler{
 		service: service,
 	}
 }
 
 type detectTextHandler struct {
-	service *services.DetectTextService
+	service services.DetectTextService
 }
 
 func (h *detectTextHandler) Get(ctx *gin.Context) {
-
+	results, err := h.service.GetResults()
+	if err != nil {
+		_ = ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, results)
 }
 
 func (h *detectTextHandler) Post(ctx *gin.Context) {
-
+	inputFile, err := ctx.FormFile("file")
+	if err != nil {
+		_ = ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	tempFile, err := utils.NewTempFile()
+	if err != nil {
+		_ = ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	defer func() {
+		_ = os.Remove(tempFile.Name())
+	}()
+	_ = ctx.SaveUploadedFile(inputFile, tempFile.Name())
+	err = h.service.DetectTexts(tempFile)
+	if err != nil {
+		_ = ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	ctx.Status(http.StatusOK)
 }
--- backend/pkg/services/detect_text_service.go
+++ backend/pkg/services/detect_text_service.go
@@ -1,11 +1,35 @@
 package services
 
+import (
+	"github.com/ijufumi/google-vision-sample/pkg/gateways/google/clients"
+	"github.com/ijufumi/google-vision-sample/pkg/models"
+	"os"
+)
+
 type DetectTextService interface {
+	GetResults() ([]models.ExtractionResult, error)
+	DetectTexts(file *os.File) error
 }
 
-func NewDetectTextService() DetectTextService {
-	return &detectTextService{}
+func NewDetectTextService(
+	storageAPIClient clients.StorageAPIClient,
+	visionAPIClient clients.VisionAPIClient,
+) DetectTextService {
+	return &detectTextService{
+		storageAPIClient: storageAPIClient,
+		visionAPIClient:  visionAPIClient,
+	}
+}
+
+func (s *detectTextService) GetResults() ([]models.ExtractionResult, error) {
+	return nil, nil
+}
+
+func (s *detectTextService) DetectTexts(file *os.File) error {
+	return nil
 }
 
 type detectTextService struct {
+	storageAPIClient clients.StorageAPIClient
+	visionAPIClient  clients.VisionAPIClient
 }
