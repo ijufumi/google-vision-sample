7402d8052c45ea42d80250cc21e60388d4fb5065
Takafumi Iju <ijufumi@gmail.com>
2023-02-07 08:06:24 +0900

    Modify response type

--- backend/pkg/models/exctraction_result.go
+++ backend/pkg/models/exctraction_result.go
@@ -9,7 +9,7 @@ type ExtractionResult struct {
 	ID        string                       `json:"id"`
 	Status    enums.ExtractionResultStatus `json:"status"`
 	ImageUri  string                       `json:"image_uri"`
-	OutputUri *string                      `json:"output_uri"`
+	OutputUri string                       `json:"output_uri"`
 	CreatedAt time.Time                    `json:"created_at"`
 	UpdatedAt time.Time                    `json:"updated_at"`
 }
