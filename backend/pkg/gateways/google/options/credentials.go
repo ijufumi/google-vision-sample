d0e6fc482398de0aba191c0e7e1bd2153e53de5f
Takafumi Iju <ijufumi@gmail.com>
2023-02-08 07:52:19 +0900

    Fixed reading credentials from json string

--- backend/pkg/gateways/google/options/credentials.go
+++ backend/pkg/gateways/google/options/credentials.go
@@ -1,15 +1,10 @@
 package options
 
 import (
-	"encoding/json"
 	"github.com/ijufumi/google-vision-sample/pkg/configs"
 	"google.golang.org/api/option"
 )
 
 func GetCredentialOption(config *configs.Config) option.ClientOption {
-	bytes, err := json.Marshal(config.Google.Credential)
-	if err != nil {
-		panic(err)
-	}
-	return option.WithCredentialsJSON(bytes)
+	return option.WithCredentialsJSON([]byte(config.Google.Credential))
 }
