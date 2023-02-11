5c59bc4732fbef24c39da2249bc760ada5162dfc
Takafumi Iju <ijufumi@gmail.com>
2023-02-11 12:57:40 +0900

    Add repository base

---
+++ frontend/src/repositories/BaseRepository.ts
@@ -0,0 +1,154 @@
+import {
+    UnexpectedError,
+    ClientError,
+    ServerError,
+} from "../components/errors";
+
+enum Methods {
+    Get = "GET",
+    Put = "PUT",
+    Post = "POST",
+    Delete = "DELETE",
+}
+
+abstract class BaseRepository {
+    private readonly apiEndpoint: string;
+
+    constructor(apiEndpoint: string) {
+        this.apiEndpoint = apiEndpoint;
+    }
+
+    _get = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+    }) => {
+        return await this._request(
+            Methods.Get,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            undefined,
+            args.headers
+        );
+    };
+
+    _delete = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+    }) => {
+        return await this._request(
+            Methods.Delete,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            undefined,
+            args.headers
+        );
+    };
+
+    _post = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+        body?: object;
+    }) => {
+        const baseHeaders = { "Content-Type": "application/json;charset=utf-8" };
+        return await this._request(
+            Methods.Post,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            args.body,
+            Object.assign(baseHeaders, args.headers ? args.headers : {})
+        );
+    };
+
+    _put = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+        body?: object;
+    }) => {
+        const baseHeaders = { "Content-Type": "application/json;charset=utf-8" };
+        return await this._request(
+            Methods.Put,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            args.body,
+            Object.assign(baseHeaders, args.headers ? args.headers : {})
+        );
+    };
+
+    _download = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+    }) => {
+        return await this._request(
+            Methods.Get,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            undefined,
+            args.headers,
+            true
+        );
+    };
+
+    _upload = async (args: {
+        path: string;
+        auth?: boolean;
+        headers?: Record<string, string>;
+        body: FormData;
+    }) => {
+        return await this._request(
+            Methods.Post,
+            this.apiEndpoint,
+            args.path,
+            args.auth,
+            args.body,
+            Object.assign({}, args.headers ? args.headers : {}),
+            false,
+            true
+        );
+    };
+
+    _request = async (
+        method: Methods,
+        apiEndpoint: string,
+        path: string,
+        auth?: boolean,
+        body?: object | FormData,
+        header?: Record<string, string>,
+        responseAsBlob?: boolean,
+        requestAsForm?: boolean
+    ) => {
+        let bodyData = undefined;
+        if (body) {
+            bodyData = requestAsForm ? (body as FormData) : JSON.stringify(body);
+        }
+        const response = await fetch(`${apiEndpoint}${path}`, {
+            method: method.toString(),
+            headers: Object.assign({}, header ? header : {}),
+            body: bodyData,
+        }).catch((e) => {
+            throw new UnexpectedError(`No response error with ${e}`);
+        });
+
+        if (response.ok) {
+            if (responseAsBlob) {
+                return response.blob();
+            }
+            return response.json();
+        }
+        if (response.status < 500) {
+            throw new ClientError(`ClientError with ${response}`);
+        }
+        throw new ServerError(`ServerError with ${response}`);
+    };
+}
+
+export default BaseRepository;
---
+++ frontend/src/repositories/ExtractionResultRepository.ts
@@ -0,0 +1,8 @@
+import BaseRepository from "./BaseRepository";
+
+export interface ExtractionResultRepository {
+
+}
+
+export class ExtractionResultRepositoryImpl extends BaseRepository implements ExtractionResultRepository {
+}
