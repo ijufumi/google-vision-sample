3096221ce3ff68394faeea03ba7b28c9fa984c74
Takafumi Iju <ijufumi@gmail.com>
2023-02-12 12:18:33 +0900

    Add models

---
+++ frontend/src/models/ExtractedText.ts
@@ -0,0 +1,36 @@
+export interface Props {
+    id: string
+    extractionResultID: string
+    text: string
+    top: number
+    bottom: number
+    left: number
+    right: number
+    createdAt: number
+    updatedAt: number
+}
+
+export default class ExtractedText {
+    readonly id: string
+    readonly extractionResultID: string
+    readonly text: string
+    readonly top: number
+    readonly bottom: number
+    readonly left: number
+    readonly right: number
+    readonly createdAt: number
+    readonly updatedAt: number
+
+
+    constructor(props: Props) {
+        this.id = props.id
+        this.extractionResultID = props.extractionResultID
+        this.text = props.text
+        this.top = props.top
+        this.bottom = props.bottom
+        this.left = props.left
+        this.right = props.right
+        this.createdAt = props.createdAt
+        this.updatedAt = props.updatedAt
+    }
+}
---
+++ frontend/src/models/ExtractionResult.ts
@@ -0,0 +1,41 @@
+import ExtractedText, {Props as ExtractedTextProps} from "./ExtractedText";
+
+export enum ExtractionResultStatus {
+    Running = "running",
+    Succeeded = "succeeded",
+    Failed = "failed"
+}
+
+export interface Props {
+    id: string
+    status: ExtractionResultStatus
+    imageUri: string
+    outputUri: string
+    createdAt: number
+    updatedAt: number
+    extractedTexts: ExtractedTextProps[]
+}
+
+export default class ExtractionResult {
+    readonly id: string
+    readonly status: ExtractionResultStatus
+    readonly imageUri: string
+    readonly outputUri: string
+    readonly createdAt: number
+    readonly updatedAt: number
+    readonly extractedTexts: ExtractedText[]
+
+    constructor(props: Props) {
+        this.id = props.id
+        this.status = props.status
+        this.imageUri = props.imageUri
+        this.outputUri = props.outputUri
+        this.createdAt = props.createdAt
+        this.updatedAt = props.updatedAt
+        if (props.extractedTexts) {
+            this.extractedTexts = props.extractedTexts.map((p => new ExtractedText(p)))
+        } else {
+            this.extractedTexts = []
+        }
+    }
+}
