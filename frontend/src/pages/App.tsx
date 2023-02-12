3987e666a5ae79f6ffc45802115acf7ef6a14a4f
Takafumi Iju <ijufumi@gmail.com>
2023-02-13 08:47:59 +0900

    [WIP] App page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,8 +1,32 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useState, useEffect } from 'react'
+import ExtractionResult from "../models/ExtractionResult";
+import ExtractionUseCaseImpl from "../usecases/ExtractionUseCase";
 
 interface Props {}
 
 const App: FC<Props> = () => {
+    const [initialized, setInitialized] = useState<boolean>(false)
+    const [extractionResults, setExtractionResults] = useState<ExtractionResult[]>([])
+
+    const useCase = new ExtractionUseCaseImpl()
+
+    useEffect(() => {
+        const initialize = async () => {
+            const _extractionResults = await useCase.getExtractionResults()
+            if (_extractionResults) {
+                setExtractionResults(_extractionResults)
+            } else {
+                console.error("something wrong...")
+            }
+            setInitialized(true)
+        }
+        initialize()
+    }, [])
+
+    if (!initialized) {
+        return null
+    }
+
     return <Fragment>
 
     </Fragment>
