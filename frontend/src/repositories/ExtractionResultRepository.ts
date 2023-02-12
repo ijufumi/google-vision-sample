860857f26b57e666ba7cd66fbe5c7e4437196f87
Takafumi Iju <ijufumi@gmail.com>
2023-02-12 12:18:46 +0900

    Add repository

--- frontend/src/repositories/ExtractionResultRepository.ts
+++ frontend/src/repositories/ExtractionResultRepository.ts
@@ -1,8 +1,28 @@
 import BaseRepository from "./BaseRepository";
+import ExtractionResult, { Props } from "../models/ExtractionResult";
 
 export interface ExtractionResultRepository {
+    getAll(): Promise<ExtractionResult[]>
 
+    getById(args: { id: string }): Promise<ExtractionResult>
+
+    create(args: { file: File }): Promise<void>
 }
 
 export class ExtractionResultRepositoryImpl extends BaseRepository implements ExtractionResultRepository {
+    create = async (args: { file: File }) => {
+        const formData = new FormData()
+        formData.set("file", args.file)
+        await this._upload({path: "/", body: formData})
+    }
+
+    getAll = async () => {
+        const results = await this._get({path: "/"})
+        return results.map((p: Props) => new ExtractionResult(p))
+    }
+
+    getById = async (args: { id: string }) => {
+        const result = await this._get({path: `/${args.id}`})
+        return new ExtractionResult(result)
+    }
 }
