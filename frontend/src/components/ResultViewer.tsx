fd4dc4707d0bb2d1e63e057ee0eee144bf3e7ec1
Takafumi Iju <ijufumi@gmail.com>
2023-02-19 08:43:25 +0900

    [WIP] Result dialog

--- frontend/src/components/ResultViewer.tsx
+++ frontend/src/components/ResultViewer.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useMemo, useEffect, useState } from 'react'
 import { Dialog } from "evergreen-ui"
+import ExtractionResult from "../models/ExtractionResult"
+import ExtractionUseCaseImpl from "../usecases/ExtractionUseCase"
 
 export interface Props {
   extractionResultId: string
@@ -7,8 +9,27 @@ export interface Props {
 }
 
 const ResultViewer: FC<Props> = ({ extractionResultId, onClose }) => {
+  const [initialized, setInitialized] = useState<boolean>(false)
+  const [extractionResult, setExtractionResult] = useState<ExtractionResult|undefined>(undefined)
   const isShown = useMemo(() => !!extractionResultId, [extractionResultId])
 
+  const useCase = new ExtractionUseCaseImpl()
+
+  useEffect(() => {
+    initialize()
+  })
+
+  const initialize = async () => {
+    const _extractionResult = await useCase.getExtractionResult(extractionResultId)
+    if (_extractionResult) {
+      setExtractionResult(_extractionResult)
+    }
+    setInitialized(true)
+  }
+
+  if (!initialized) {
+    return null
+  }
 
   return (
     <Dialog
