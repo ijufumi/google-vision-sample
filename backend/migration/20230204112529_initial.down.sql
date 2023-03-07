8e6ebe15165069fffb71ec7f58eba5b01fe3c5ae
Takafumi Iju <ijufumi@gmail.com>
2023-03-08 08:27:51 +0900

    Restructured tables

--- backend/migration/20230204112529_initial.down.sql
+++ backend/migration/20230204112529_initial.down.sql
@@ -1,6 +1,9 @@
+drop index if exists idx_files_extraction_result_id;
+drop index if exists idx_files_deleted_at;
 drop index if exists idx_extracted_texts_extraction_result_id;
 drop index if exists idx_extracted_texts_deleted_at;
-drop index if exists idx_extraction_results_deleted_at;
+drop index if exists idx_jobs_deleted_at;
 
+drop table if exists files;
 drop table if exists extracted_texts;
-drop table if exists extraction_results;
+drop table if exists jobs;
--- backend/migration/20230204112529_initial.up.sql
+++ backend/migration/20230204112529_initial.up.sql
@@ -1,4 +1,4 @@
-create table if not exists extraction_results
+create table if not exists jobs
 (
     id         varchar(26)  not null primary key,
     status     varchar(10)  not null,
@@ -9,21 +9,38 @@ create table if not exists extraction_results
     deleted_at timestamp
 );
 
-create index if not exists idx_extraction_results_deleted_at on extraction_results (deleted_at);
+create index if not exists idx_jobs_deleted_at on jobs (deleted_at);
 
 create table if not exists extracted_texts
 (
-    id                   varchar(26)      not null primary key,
-    extraction_result_id varchar(26)      not null references extraction_results (id),
-    text                 varchar(255)     not null,
-    top                  double precision not null,
-    bottom               double precision not null,
-    "left"               double precision not null,
-    "right"              double precision not null,
-    created_at           timestamp        not null,
-    updated_at           timestamp        not null,
-    deleted_at           timestamp
+    id                varchar(26)      not null primary key,
+    extracted_text_id varchar(26)      not null references jobs (id),
+    text              varchar(255)     not null,
+    top               double precision not null,
+    bottom            double precision not null,
+    "left"            double precision not null,
+    "right"           double precision not null,
+    created_at        timestamp        not null,
+    updated_at        timestamp        not null,
+    deleted_at        timestamp
 );
 
 create index if not exists idx_extracted_texts_deleted_at on extracted_texts (deleted_at);
-create index if not exists idx_extracted_texts_extraction_result_id on extracted_texts (extraction_result_id);
+create index if not exists idx_extracted_texts_extraction_result_id on extracted_texts (extracted_text_id);
+
+create table if not exists files
+(
+    id                varchar(26)  not null primary key,
+    extracted_text_id varchar(26)  not null references jobs (id),
+    is_output         boolean      not null default 1,
+    file_key          varchar(255) not null,
+    size              integer      not null default 0,
+    content_type      varchar(100) not null default 'application/octet-stream',
+    output_key        varchar(255),
+    created_at        timestamp    not null,
+    updated_at        timestamp    not null,
+    deleted_at        timestamp
+);
+
+create index if not exists idx_files_deleted_at on files (deleted_at);
+create index if not exists idx_files_extraction_result_id on files (extracted_text_id);
