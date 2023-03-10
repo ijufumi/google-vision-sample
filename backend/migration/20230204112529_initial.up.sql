01540e73e87233f8927847c8c2ab37c1b1cd221a
Takafumi Iju <ijufumi@gmail.com>
2023-03-11 08:24:31 +0900

    Fixed DDL

--- backend/migration/20230204112529_initial.up.sql
+++ backend/migration/20230204112529_initial.up.sql
@@ -11,35 +11,35 @@ create index if not exists idx_jobs_deleted_at on jobs (deleted_at);
 
 create table if not exists extracted_texts
 (
-    id                varchar(26)      not null primary key,
-    extracted_text_id varchar(26)      not null references jobs (id),
-    text              varchar(255)     not null,
-    top               double precision not null,
-    bottom            double precision not null,
-    "left"            double precision not null,
-    "right"           double precision not null,
-    created_at        timestamp        not null,
-    updated_at        timestamp        not null,
-    deleted_at        timestamp
+    id         varchar(26)      not null primary key,
+    job_id     varchar(26)      not null references jobs (id),
+    text       varchar(255)     not null,
+    top        double precision not null,
+    bottom     double precision not null,
+    "left"     double precision not null,
+    "right"    double precision not null,
+    created_at timestamp        not null,
+    updated_at timestamp        not null,
+    deleted_at timestamp
 );
 
 create index if not exists idx_extracted_texts_deleted_at on extracted_texts (deleted_at);
-create index if not exists idx_extracted_texts_extraction_result_id on extracted_texts (extracted_text_id);
+create index if not exists idx_extracted_texts_job_id on extracted_texts (job_id);
 
 create table if not exists job_files
 (
-    id                varchar(26)  not null primary key,
-    extracted_text_id varchar(26)  not null references jobs (id),
-    is_output         boolean      not null default false,
-    file_key          varchar(255) not null,
-    file_name         varchar(255) not null,
-    size              integer      not null default 0,
-    content_type      varchar(100) not null default 'application/octet-stream',
-    output_key        varchar(255),
-    created_at        timestamp    not null,
-    updated_at        timestamp    not null,
-    deleted_at        timestamp
+    id           varchar(26)  not null primary key,
+    job_id       varchar(26)  not null references jobs (id),
+    is_output    boolean      not null default false,
+    file_key     varchar(255) not null,
+    file_name    varchar(255) not null,
+    size         integer      not null default 0,
+    content_type varchar(100) not null default 'application/octet-stream',
+    output_key   varchar(255),
+    created_at   timestamp    not null,
+    updated_at   timestamp    not null,
+    deleted_at   timestamp
 );
 
 create index if not exists idx_job_files_deleted_at on job_files (deleted_at);
-create index if not exists idx_job_files_extraction_result_id on job_files (extracted_text_id);
+create index if not exists idx_job_files_job_id on job_files (job_id);
