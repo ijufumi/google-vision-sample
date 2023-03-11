9d5f431f69efc92d6c84fb1e5172da7ef05ce6f8
Takafumi Iju <ijufumi@gmail.com>
2023-03-12 08:58:20 +0900

    Modified to const from let

--- frontend/src/components/files.ts
+++ frontend/src/components/files.ts
@@ -1,7 +1,7 @@
 export const readAsFile = async (url: string) => {
   const response = await fetch(url)
   const data = await response.blob()
-  let metadata = {
+  const metadata = {
     type: data.type,
   }
   return new File([data], data.name, metadata)
