6c95ddb8a866b07169021b29185254e989316f1f
Takafumi Iju <ijufumi@gmail.com>
2023-03-22 07:45:07 +0900

    Added max min function

--- backend/pkg/utils/arrays.go
+++ backend/pkg/utils/arrays.go
@@ -19,3 +19,16 @@ func MaxInArray(array ...float64) float64 {
 	}
 	return value
 }
+
+func MaxMinInArray(array ...float64) (max float64, min float64) {
+	max = array[0]
+	min = array[0]
+	for _, v := range array[1:] {
+		if max < v {
+			max = v
+		} else if min > v {
+			min = v
+		}
+	}
+	return
+}
