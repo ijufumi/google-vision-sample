1312b91febd90f2d07c7aad1c6a6e4076b8bbd47
Takafumi Iju <ijufumi@gmail.com>
2023-03-21 16:56:32 +0900

    [WIP] Adjust points

--- backend/pkg/services/image_conversion_service.go
+++ backend/pkg/services/image_conversion_service.go
@@ -1,6 +1,12 @@
 package services
 
-import "os/exec"
+import (
+	"fmt"
+	"github.com/ijufumi/google-vision-sample/pkg/gateways/google/models"
+	"go.uber.org/zap"
+	"math"
+	"os/exec"
+)
 
 type Orientation string
 
@@ -31,18 +37,25 @@ var orientationMap = map[string]Orientation{
 
 type ImageConversionService interface {
 	DetectOrientation(filePath string) (Orientation, error)
-	ConvertPoints(points []float64, orientation Orientation) []float64
+	ConvertPoints(points []models.Vertices, orientation Orientation) [][]float64
 }
 
 type imageConversionService struct {
+	logger *zap.Logger
 }
 
-func NewImageConversionService() ImageConversionService {
-	return &imageConversionService{}
+func NewImageConversionService(logger *zap.Logger) ImageConversionService {
+	return &imageConversionService{
+		logger: logger,
+	}
 }
 
 func (s *imageConversionService) DetectOrientation(filePath string) (Orientation, error) {
-	result, err := exec.Command(identifyCommand[0], identifyCommand[1:]...).Output()
+	args := identifyCommand[1:]
+	args = append(args, filePath)
+	result, err := exec.Command(identifyCommand[0], args...).Output()
+
+	s.logger.Info(fmt.Sprintf("result: %s", string(result)))
 	if err != nil {
 		return Orientation_None, err
 	}
@@ -54,25 +67,42 @@ func (s *imageConversionService) DetectOrientation(filePath string) (Orientation
 	return Orientation_None, nil
 }
 
-func (s *imageConversionService) ConvertPoints(points []float64, orientation Orientation) []float64 {
+func (s *imageConversionService) ConvertPoints(points []models.Vertices, orientation Orientation) [][]float64 {
 	if len(points) != 4 {
-		return points
+		return [][]float64{}
 	}
-	x := points[0]
-	y := points[1]
-	left := points[2]
-	bottom := points[3]
+	p1 := []float64{points[0].X, points[0].Y}
+	p2 := []float64{points[1].X, points[1].Y}
+	p3 := []float64{points[2].X, points[2].Y}
+	p4 := []float64{points[3].X, points[3].Y}
+	m := []float64{(p3[0] - p1[0]) / 2, (p3[1] - p1[1]) / 2}
 
+	angle := float64(0)
 	switch orientation {
-	case Orientation_TopRight:
-	case Orientation_BottomRight:
 	case Orientation_BottomLeft:
-	case Orientation_LeftTop:
+		angle = 180
 	case Orientation_RightTop:
-	case Orientation_RightBottom:
+		angle = 270
 	case Orientation_LeftBottom:
-	case Orientation_None, Orientation_TopLeft:
+		angle = 90
+	default:
 		// nothing
 	}
-	return []float64{x, y, left, bottom}
+
+	p1 = s.convertPoint(p1, angle, m)
+	p2 = s.convertPoint(p2, angle, m)
+	p3 = s.convertPoint(p3, angle, m)
+	p4 = s.convertPoint(p4, angle, m)
+
+	return [][]float64{p1, p2, p3, p4}
+}
+
+func (s *imageConversionService) convertPoint(point []float64, angle float64, middlePoint []float64) []float64 {
+	sin, cos := math.Sincos(angle)
+
+	adjustPoint := []float64{point[0] - middlePoint[0], point[1] - middlePoint[1]}
+	adjustPoint[0] = adjustPoint[0]*cos - adjustPoint[1]*sin
+	adjustPoint[1] = adjustPoint[0]*sin - adjustPoint[1]*cos
+
+	return []float64{adjustPoint[0] + middlePoint[0], adjustPoint[1] + middlePoint[1]}
 }
