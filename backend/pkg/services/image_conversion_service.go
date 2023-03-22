6cf42f06120a00189112efd825792ed53e50b6f9
Takafumi Iju <ijufumi@gmail.com>
2023-03-23 07:37:18 +0900

    Fixed calculation of middle point

--- backend/pkg/services/image_conversion_service.go
+++ backend/pkg/services/image_conversion_service.go
@@ -85,7 +85,7 @@ func (s *imageConversionService) ConvertPoints(points []models.Vertices, orienta
 	p4 := []float64{points[3].X, points[3].Y}
 	x2, x1 := utils.MaxMinInArray(points[0].X, points[1].X, points[2].X, points[3].X)
 	y2, y1 := utils.MaxMinInArray(points[0].Y, points[1].Y, points[2].Y, points[3].Y)
-	m := []float64{(x2 - x1) / 2, (y2 - y1) / 2}
+	m := []float64{(x1 + x2) / 2, (y1 + y2) / 2}
 
 	angle := float64(0)
 	switch orientation {
