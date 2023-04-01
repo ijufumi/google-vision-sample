e25909a727a5392672a20f12b35e726a89a1d079
Takafumi Iju <ijufumi@gmail.com>
2023-04-02 07:50:23 +0900

    Fixed ddl

--- backend/migration/20230204112529_initial.up.sql
+++ backend/migration/20230204112529_initial.up.sql
@@ -9,23 +9,6 @@ create table if not exists jobs
 
 create index if not exists idx_jobs_deleted_at on jobs (deleted_at);
 
-create table if not exists extracted_texts
-(
-    id          varchar(26)      not null primary key,
-    job_file_id varchar(26)      not null references job_files (id),
-    text        varchar(255)     not null,
-    top         double precision not null,
-    bottom      double precision not null,
-    "left"      double precision not null,
-    "right"     double precision not null,
-    created_at  timestamp        not null,
-    updated_at  timestamp        not null,
-    deleted_at  timestamp
-);
-
-create index if not exists idx_extracted_texts_deleted_at on extracted_texts (deleted_at);
-create index if not exists idx_extracted_texts_job_file_id on extracted_texts (job_file_id);
-
 create table if not exists job_files
 (
     id           varchar(26)  not null primary key,
@@ -44,3 +27,20 @@ create table if not exists job_files
 
 create index if not exists idx_job_files_deleted_at on job_files (deleted_at);
 create index if not exists idx_job_files_job_id on job_files (job_id);
+
+create table if not exists extracted_texts
+(
+    id          varchar(26)      not null primary key,
+    job_file_id varchar(26)      not null references job_files (id),
+    text        varchar(255)     not null,
+    top         double precision not null,
+    bottom      double precision not null,
+    "left"      double precision not null,
+    "right"     double precision not null,
+    created_at  timestamp        not null,
+    updated_at  timestamp        not null,
+    deleted_at  timestamp
+);
+
+create index if not exists idx_extracted_texts_deleted_at on extracted_texts (deleted_at);
+create index if not exists idx_extracted_texts_job_file_id on extracted_texts (job_file_id);
