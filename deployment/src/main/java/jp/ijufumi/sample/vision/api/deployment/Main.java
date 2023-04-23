983b5dd4c0811183faba54ffc632c813adaa3135
Takafumi Iju <ijufumi@gmail.com>
2023-04-24 07:07:48 +0900

    Reformat code

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/Main.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/Main.java
@@ -3,13 +3,12 @@ package jp.ijufumi.sample.vision.api.deployment;
 import com.hashicorp.cdktf.App;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 
+public class Main {
 
-public class Main
-{
-    public static void main(String[] args) {
-        final var app = new App();
-        final var config = Config.read();
-        new MainStack(app, "deployment", config);
-        app.synth();
-    }
+  public static void main(String[] args) {
+    final var app = new App();
+    final var config = Config.read();
+    new MainStack(app, "deployment", config);
+    app.synth();
+  }
 }
