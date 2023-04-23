e2af7ea1a66b22fe15cbad8f11f47bc199397d44
Takafumi Iju <ijufumi@gmail.com>
2023-04-23 18:03:43 +0900

    [WIP] cloud cdn

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudCDNStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudCDNStack.java
@@ -1,16 +1,29 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
+import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucket;
+import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucketCdnPolicy;
+import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucketCdnPolicyCacheKeyPolicy;
+import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucketConfig;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
-import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucket;
-import com.hashicorp.cdktf.providers.google.compute_backend_bucket.ComputeBackendBucketConfig;
 
 public class CloudCDNStack {
-    public static void create(final Construct scope, final Config config) {
-        var backendBucketConfig = ComputeBackendBucketConfig
-            .builder()
-            .bucketName(config.BucketName())
-            .build();
-        var backendBucket = new ComputeBackendBucket(scope, "backend-bucket", backendBucketConfig);
-    }
+
+  public static void create(final Construct scope, final Config config) {
+    var backendBucketCdnPolicyCacheKeyPolicy = ComputeBackendBucketCdnPolicyCacheKeyPolicy.builder()
+        .build();
+    var backendBucketCdnPolicy = ComputeBackendBucketCdnPolicy
+        .builder()
+        .cacheKeyPolicy(backendBucketCdnPolicyCacheKeyPolicy)
+        .cacheMode("CACHE_ALL_STATIC")
+        .build();
+    var backendBucketConfig = ComputeBackendBucketConfig
+        .builder()
+        .bucketName(config.BucketName())
+        .enableCdn(true)
+        .cdnPolicy(backendBucketCdnPolicy)
+        .compressionMode("AUTOMATIC")
+        .build();
+    new ComputeBackendBucket(scope, "backend-bucket", backendBucketConfig);
+  }
 }
