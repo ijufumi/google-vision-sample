8ec1be911bda93ee08aff1d8dccc2f74748fe2bd
Takafumi Iju <ijufumi@gmail.com>
2023-04-23 17:37:24 +0900

    Reformat code

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/MainStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/MainStack.java
@@ -1,28 +1,29 @@
 package jp.ijufumi.sample.vision.api.deployment;
 
+import com.hashicorp.cdktf.TerraformStack;
+import com.hashicorp.cdktf.providers.google.provider.GoogleProvider;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import jp.ijufumi.sample.vision.api.deployment.stacks.ArtifactRegistryStack;
 import jp.ijufumi.sample.vision.api.deployment.stacks.BucketStack;
 import jp.ijufumi.sample.vision.api.deployment.stacks.CloudCDNStack;
 import jp.ijufumi.sample.vision.api.deployment.stacks.CloudRunStack;
 import software.constructs.Construct;
-import com.hashicorp.cdktf.providers.google.provider.GoogleProvider;
-import com.hashicorp.cdktf.TerraformStack;
 
-public class MainStack extends TerraformStack
-{
-    public MainStack(final Construct scope, final String id, final Config config) {
-        super(scope, id);
+public class MainStack extends TerraformStack {
 
-        GoogleProvider
-                .Builder
-                .create(this, "gcp-provider")
-                .region(config.Region())
-                .project(config.ProjectId())
-                .build();
-        CloudRunStack.create(this, config);
-        BucketStack.create(this, config);
-        ArtifactRegistryStack.create(this, config);
-        CloudCDNStack.create(this, config);
-    }
+  public MainStack(final Construct scope, final String id, final Config config) {
+    super(scope, id);
+
+    GoogleProvider
+        .Builder
+        .create(this, "gcp-provider")
+        .region(config.Region())
+        .project(config.ProjectId())
+        .build();
+
+    CloudRunStack.create(this, config);
+    BucketStack.create(this, config);
+    ArtifactRegistryStack.create(this, config);
+    CloudCDNStack.create(this, config);
+  }
 }
