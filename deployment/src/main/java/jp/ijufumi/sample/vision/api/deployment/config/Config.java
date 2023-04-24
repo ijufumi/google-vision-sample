025a4666055ec2ad37534dc8600eaf3cd55e5a9e
Takafumi Iju <ijufumi@gmail.com>
2023-04-25 07:06:20 +0900

    Added port to cloud run

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/config/Config.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/config/Config.java
@@ -2,77 +2,81 @@ package jp.ijufumi.sample.vision.api.deployment.config;
 
 
 import io.github.cdimascio.dotenv.Dotenv;
-
 import java.util.List;
 
 public class Config {
 
-    private final Dotenv dotenv;
-    private Config(Dotenv dotenv) {
-        this.dotenv = dotenv;
-    }
+  private final Dotenv dotenv;
 
-    public String getEnv(String key, String defaultValue) {
-        return this.dotenv.get(key, defaultValue);
-    }
+  private Config(Dotenv dotenv) {
+    this.dotenv = dotenv;
+  }
 
-    public String getEnv(String key) {
-        return this.getEnv(key, (String) null);
-    }
+  public String getEnv(String key, String defaultValue) {
+    return this.dotenv.get(key, defaultValue);
+  }
 
-    public Integer getEnv(String key, Integer defaultValue) {
-        try {
-            return Integer.parseInt(this.dotenv.get(key));
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-            return defaultValue;
-        }
-    }
+  public String getEnv(String key) {
+    return this.getEnv(key, (String) null);
+  }
 
-    public String ProjectId() {
-        return this.getEnv("PROJECT_ID", "");
+  public Integer getEnv(String key, Integer defaultValue) {
+    try {
+      return Integer.parseInt(this.dotenv.get(key));
+    } catch (NumberFormatException e) {
+      System.out.println(e);
+      return defaultValue;
     }
+  }
+  
+  public String ProjectId() {
+    return this.getEnv("PROJECT_ID", "");
+  }
 
-    public String Region() {
-        return this.getEnv("REGION", "");
-    }
+  public String Region() {
+    return this.getEnv("REGION", "");
+  }
 
-    public String Location() {
-        return this.getEnv("LOCATION", "");
-    }
+  public String Location() {
+    return this.getEnv("LOCATION", "");
+  }
 
-    public String BucketName() {
-        return this.getEnv("BUCKET_NAME", "");
-    }
+  public String BucketName() {
+    return this.getEnv("BUCKET_NAME", "");
+  }
 
-    public List<String> BucketCorsMethods() {
-        return List.of(this.getEnv("BUCKET_CORS_METHODS", "*").split(","));
-    }
+  public List<String> BucketCorsMethods() {
+    return List.of(this.getEnv("BUCKET_CORS_METHODS", "*").split(","));
+  }
 
-    public List<String> BucketCorsOrigins() {
-        return List.of(this.getEnv("BUCKET_CORS_ORIGINS", "*").split(","));
-    }
+  public List<String> BucketCorsOrigins() {
+    return List.of(this.getEnv("BUCKET_CORS_ORIGINS", "*").split(","));
+  }
 
-    public Integer BucketCorsMaxAge() {
-        return this.getEnv("BUCKET_CORS_MAX_AGE", 3600);
-    }
+  public Integer BucketCorsMaxAge() {
+    return this.getEnv("BUCKET_CORS_MAX_AGE", 3600);
+  }
 
-    public String CloudRunContainerImage() {
-        return this.getEnv("CLOUD_RUN_CONTAINER_IMAGE");
-    }
+  public String CloudRunContainerImage() {
+    return this.getEnv("CLOUD_RUN_CONTAINER_IMAGE");
+  }
 
-    public String RepositoryId() {
-        return this.getEnv("REPOSITORY_ID");
-    }
+  public Integer CloudRunContainerPort() {
+    return this.getEnv("CLOUD_RUN_CONTAINER_PORT", 0);
+  }
 
-    public static Config read() {
-        var dotenv = Dotenv
-                .configure()
-                .systemProperties()
-                .ignoreIfMalformed()
-                .ignoreIfMissing()
-                .load();
+  public String RepositoryId() {
+    return this.getEnv("REPOSITORY_ID");
+  }
 
-        return new Config(dotenv);
-    }
+  public static Config read() {
+    var dotenv = Dotenv
+        .configure()
+        .systemProperties()
+        .ignoreIfMalformed()
+        .ignoreIfMissing()
+        .load();
+
+    return new Config(dotenv);
+  }
 }
--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudRunStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudRunStack.java
@@ -4,6 +4,7 @@ import com.hashicorp.cdktf.providers.google.cloud_run_v2_service.CloudRunV2Servi
 import com.hashicorp.cdktf.providers.google.cloud_run_v2_service.CloudRunV2ServiceConfig;
 import com.hashicorp.cdktf.providers.google.cloud_run_v2_service.CloudRunV2ServiceTemplate;
 import com.hashicorp.cdktf.providers.google.cloud_run_v2_service.CloudRunV2ServiceTemplateContainers;
+import com.hashicorp.cdktf.providers.google.cloud_run_v2_service.CloudRunV2ServiceTemplateContainersPorts;
 import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
@@ -11,9 +12,14 @@ import software.constructs.Construct;
 public class CloudRunStack {
 
   public static void create(final Construct scope, final Config config) {
+    var containerPort = CloudRunV2ServiceTemplateContainersPorts
+        .builder()
+        .containerPort(config.CloudRunContainerPort())
+        .build();
     var container = CloudRunV2ServiceTemplateContainers
         .builder()
         .image(config.CloudRunContainerImage())
+        .ports(List.of(containerPort))
         .build();
     var template = CloudRunV2ServiceTemplate
         .builder()
