3bfc69b2a5aa6074575fbd527108f122a4f22a3f
Takafumi Iju <ijufumi@gmail.com>
2023-05-11 08:00:01 +0900

    Added settings to database

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
@@ -2,17 +2,23 @@ package jp.ijufumi.sample.vision.api.deployment.stacks;
 
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstance;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceConfig;
+import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettings;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
 
 public class DatabaseStack {
 
   public static SqlDatabaseInstance create(final Construct scope, final Config config) {
+    var databaseSetting = SqlDatabaseInstanceSettings
+        .builder()
+        .tier("db-f1-micro")
+        .build();
     var databaseConfig = SqlDatabaseInstanceConfig
         .builder()
         .project(config.ProjectId())
         .region(config.Region())
         .databaseVersion("POSTGRES_14")
+        .settings(databaseSetting)
         .build();
 
     return new SqlDatabaseInstance(scope, "sql-database-instance", databaseConfig);
