c877d605d6e36aac2964c729de7f5e4de6ccd58f
Takafumi Iju <ijufumi@gmail.com>
2023-05-15 07:53:02 +0900

    Added project service to secret manager

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/SecretManagerStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/SecretManagerStack.java
@@ -1,21 +1,31 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
+import com.hashicorp.cdktf.providers.google.project_service.ProjectService;
+import com.hashicorp.cdktf.providers.google.project_service.ProjectServiceConfig;
 import com.hashicorp.cdktf.providers.google.secret_manager_secret.SecretManagerSecret;
 import com.hashicorp.cdktf.providers.google.secret_manager_secret.SecretManagerSecretConfig;
 import com.hashicorp.cdktf.providers.google.secret_manager_secret.SecretManagerSecretReplication;
 import com.hashicorp.cdktf.providers.google.secret_manager_secret_version.SecretManagerSecretVersion;
 import com.hashicorp.cdktf.providers.google.secret_manager_secret_version.SecretManagerSecretVersionConfig;
+import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
 
 public class SecretManagerStack {
 
   public static SecretManagerSecretVersion create(final Construct scope, final Config config) {
+    var serviceConfig = ProjectServiceConfig
+        .builder()
+        .project(config.ProjectId())
+        .service("secretmanager.googleapis.com")
+        .build();
+    var service = new ProjectService(scope, "project-service", serviceConfig);
     var replication = SecretManagerSecretReplication.builder().automatic(true).build();
     var secretConfig = SecretManagerSecretConfig
         .builder()
         .project(config.ProjectId())
         .secretId("google-credential")
+        .dependsOn(List.of(service))
         .replication(replication)
         .build();
     var secret = new SecretManagerSecret(scope, "secret-credential", secretConfig);
