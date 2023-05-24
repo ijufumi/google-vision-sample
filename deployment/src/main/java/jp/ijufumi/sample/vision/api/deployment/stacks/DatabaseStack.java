74d79768dc42331c783ec2b23ab8062ea5a34adf
Takafumi Iju <ijufumi@gmail.com>
2023-05-25 08:38:35 +0900

    Added private network for SQLDatabase.

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
@@ -1,18 +1,37 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
+import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetwork;
+import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetworkConfig;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstance;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceConfig;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettings;
+import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettingsIpConfiguration;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
 
 public class DatabaseStack {
 
   public static SqlDatabaseInstance create(final Construct scope, final Config config) {
+    var networkConfig = ComputeNetworkConfig
+        .builder()
+        .project(config.ProjectId())
+        .build();
+    
+    var network = new ComputeNetwork(scope, "private-network", networkConfig);
+
+    var ipConfiguration = SqlDatabaseInstanceSettingsIpConfiguration
+        .builder()
+        .ipv4Enabled(false)
+        .enablePrivatePathForGoogleCloudServices(true)
+        .privateNetwork(network.getId())
+        .build();
+
     var databaseSetting = SqlDatabaseInstanceSettings
         .builder()
         .tier("db-f1-micro")
+        .ipConfiguration(ipConfiguration)
         .build();
+
     var databaseConfig = SqlDatabaseInstanceConfig
         .builder()
         .project(config.ProjectId())
