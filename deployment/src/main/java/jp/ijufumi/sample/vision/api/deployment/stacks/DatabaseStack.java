377139dc8a18b40c2b7b68f4331022de2dc5330a
Takafumi Iju <ijufumi@gmail.com>
2023-05-29 07:28:09 +0900

    Modified network configuration.

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
@@ -1,11 +1,17 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
+import com.hashicorp.cdktf.providers.google.compute_global_address.ComputeGlobalAddress;
+import com.hashicorp.cdktf.providers.google.compute_global_address.ComputeGlobalAddressConfig;
 import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetwork;
 import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetworkConfig;
+import com.hashicorp.cdktf.providers.google.service_networking_connection.ServiceNetworkingConnection;
+import com.hashicorp.cdktf.providers.google.service_networking_connection.ServiceNetworkingConnectionConfig;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstance;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceConfig;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettings;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettingsIpConfiguration;
+import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettingsIpConfigurationAuthorizedNetworks;
+import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
 
@@ -20,11 +26,36 @@ public class DatabaseStack {
 
     var network = new ComputeNetwork(scope, "private-network", networkConfig);
 
+    var computeGlobalAddressConfig = ComputeGlobalAddressConfig
+        .builder()
+        .purpose("VPC_PEERING")
+        .addressType("INTERNAL")
+        .build();
+
+    var computeGlobalAddress = new ComputeGlobalAddress(scope, "compute-global-address",
+        computeGlobalAddressConfig);
+
+    var serviceNetworkingConnectionConfig = ServiceNetworkingConnectionConfig
+        .builder()
+        .network(network.getId())
+        .service("servicenetworking.googleapis.com")
+        .reservedPeeringRanges(List.of(computeGlobalAddress.getAddress()))
+        .build();
+
+    var serviceNetworkingConnection = new ServiceNetworkingConnection(scope,
+        "service-networking-connection", serviceNetworkingConnectionConfig);
+
+    var authorizedNetwork = SqlDatabaseInstanceSettingsIpConfigurationAuthorizedNetworks
+        .builder()
+        .name("authorized-network")
+        .build();
+
     var ipConfiguration = SqlDatabaseInstanceSettingsIpConfiguration
         .builder()
         .ipv4Enabled(false)
         .enablePrivatePathForGoogleCloudServices(true)
         .privateNetwork(network.getId())
+        .authorizedNetworks(List.of(authorizedNetwork))
         .build();
 
     var databaseSetting = SqlDatabaseInstanceSettings
@@ -40,6 +71,7 @@ public class DatabaseStack {
         .databaseVersion("POSTGRES_14")
         .settings(databaseSetting)
         .deletionProtection(false)
+        .dependsOn(List.of(serviceNetworkingConnection))
         .build();
 
     return new SqlDatabaseInstance(scope, "sql-database-instance", databaseConfig);
