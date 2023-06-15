f70b3624e9736ccb66025131aa21b862c51d852c
Takafumi Iju <ijufumi@gmail.com>
2023-06-16 06:59:53 +0900

    fix: added path matcher rule

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudCDNStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudCDNStack.java
@@ -12,6 +12,8 @@ import com.hashicorp.cdktf.providers.google.compute_target_http_proxy.ComputeTar
 import com.hashicorp.cdktf.providers.google.compute_url_map.ComputeUrlMap;
 import com.hashicorp.cdktf.providers.google.compute_url_map.ComputeUrlMapConfig;
 import com.hashicorp.cdktf.providers.google.compute_url_map.ComputeUrlMapHostRule;
+import com.hashicorp.cdktf.providers.google.compute_url_map.ComputeUrlMapPathMatcher;
+import com.hashicorp.cdktf.providers.google.compute_url_map.ComputeUrlMapPathMatcherPathRule;
 import com.hashicorp.cdktf.providers.google.storage_bucket.StorageBucket;
 import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
@@ -38,10 +40,21 @@ public class CloudCDNStack {
         .build();
     var backendBucket = new ComputeBackendBucket(scope, "backend-bucket", backendBucketConfig);
 
+    var pathMatcherRule = ComputeUrlMapPathMatcherPathRule
+        .builder()
+        .paths(List.of("*"))
+        .service(backendBucket.getId())
+        .build();
+    var pathMatcher = ComputeUrlMapPathMatcher
+        .builder()
+        .pathRule(List.of(pathMatcherRule))
+        .name("allPaths")
+        .defaultService(backendBucket.getId())
+        .build();
     var hostRule = ComputeUrlMapHostRule
         .builder()
         .hosts(List.of("*"))
-        .pathMatcher("allpaths")
+        .pathMatcher(pathMatcher.getName())
         .build();
     var urlMapConfig = ComputeUrlMapConfig
         .builder()
