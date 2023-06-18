1874ce14d8f2a094c406feed59e060fe0ce6cef4
Takafumi Iju <ijufumi@gmail.com>
2023-06-18 17:23:44 +0900

    fix: added iam member to bucket.

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/BucketStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/BucketStack.java
@@ -3,6 +3,8 @@ package jp.ijufumi.sample.vision.api.deployment.stacks;
 import com.hashicorp.cdktf.providers.google.storage_bucket.StorageBucket;
 import com.hashicorp.cdktf.providers.google.storage_bucket.StorageBucketConfig;
 import com.hashicorp.cdktf.providers.google.storage_bucket.StorageBucketCors;
+import com.hashicorp.cdktf.providers.google.storage_bucket_iam_member.StorageBucketIamMember;
+import com.hashicorp.cdktf.providers.google.storage_bucket_iam_member.StorageBucketIamMemberConfig;
 import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
@@ -24,6 +26,16 @@ public class BucketStack {
         .cors(List.of(bucketCors))
         .forceDestroy(true)
         .build();
-    return new StorageBucket(scope, "storage-bucket", bucketConfig);
+    var bucket = new StorageBucket(scope, "storage-bucket", bucketConfig);
+
+    var bucketIamMemberConfig = StorageBucketIamMemberConfig
+        .builder()
+        .bucket(bucket.getName())
+        .member("allUsers")
+        .role("roles/storage.legacyObjectReader")
+        .build();
+    new StorageBucketIamMember(scope, "storage-bucket-iam", bucketIamMemberConfig);
+
+    return bucket;
   }
 }
