6f6c37a421a06cdea430e181b24834a835b3feb1
Takafumi Iju <ijufumi@gmail.com>
2023-06-24 10:35:02 +0900

    fix: enable public address

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/DatabaseStack.java
@@ -1,11 +1,5 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
-import com.hashicorp.cdktf.providers.google.compute_global_address.ComputeGlobalAddress;
-import com.hashicorp.cdktf.providers.google.compute_global_address.ComputeGlobalAddressConfig;
-import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetwork;
-import com.hashicorp.cdktf.providers.google.compute_network.ComputeNetworkConfig;
-import com.hashicorp.cdktf.providers.google.service_networking_connection.ServiceNetworkingConnection;
-import com.hashicorp.cdktf.providers.google.service_networking_connection.ServiceNetworkingConnectionConfig;
 import com.hashicorp.cdktf.providers.google.sql_database.SqlDatabase;
 import com.hashicorp.cdktf.providers.google.sql_database.SqlDatabaseConfig;
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstance;
@@ -14,48 +8,15 @@ import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseIns
 import com.hashicorp.cdktf.providers.google.sql_database_instance.SqlDatabaseInstanceSettingsIpConfiguration;
 import com.hashicorp.cdktf.providers.google.sql_user.SqlUser;
 import com.hashicorp.cdktf.providers.google.sql_user.SqlUserConfig;
-import java.util.List;
 import jp.ijufumi.sample.vision.api.deployment.config.Config;
 import software.constructs.Construct;
 
 public class DatabaseStack {
 
   public static SqlDatabaseInstance create(final Construct scope, final Config config) {
-    var networkConfig = ComputeNetworkConfig
-        .builder()
-        .project(config.ProjectId())
-        .name("private-network")
-        .build();
-
-    var network = new ComputeNetwork(scope, "private-network", networkConfig);
-
-    var computeGlobalAddressConfig = ComputeGlobalAddressConfig
-        .builder()
-        .name("global-access")
-        .purpose("VPC_PEERING")
-        .addressType("INTERNAL")
-        .prefixLength(16)
-        .network(network.getId())
-        .build();
-
-    var computeGlobalAddress = new ComputeGlobalAddress(scope, "db-compute-global-address",
-        computeGlobalAddressConfig);
-
-    var serviceNetworkingConnectionConfig = ServiceNetworkingConnectionConfig
-        .builder()
-        .network(network.getId())
-        .service("servicenetworking.googleapis.com")
-        .reservedPeeringRanges(List.of(computeGlobalAddress.getName()))
-        .build();
-
-    var serviceNetworkingConnection = new ServiceNetworkingConnection(scope,
-        "service-networking-connection", serviceNetworkingConnectionConfig);
-
     var ipConfiguration = SqlDatabaseInstanceSettingsIpConfiguration
         .builder()
-        .ipv4Enabled(false)
-        .enablePrivatePathForGoogleCloudServices(true)
-        .privateNetwork(network.getId())
+        .ipv4Enabled(true)
         .build();
 
     var databaseSetting = SqlDatabaseInstanceSettings
@@ -71,7 +32,6 @@ public class DatabaseStack {
         .databaseVersion("POSTGRES_14")
         .settings(databaseSetting)
         .deletionProtection(false)
-        .dependsOn(List.of(serviceNetworkingConnection))
         .build();
 
     var databaseInstance = new SqlDatabaseInstance(scope, "sql-database-instance",
@@ -92,7 +52,7 @@ public class DatabaseStack {
         .build();
 
     new SqlDatabase(scope, "sql-database", databaseConfig);
-    
+
     return databaseInstance;
   }
 }
