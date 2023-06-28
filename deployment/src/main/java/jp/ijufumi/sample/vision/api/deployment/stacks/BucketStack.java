0a65943543da91d6a28d67f78ce9ea11efc3b3b2
Takafumi Iju <ijufumi@gmail.com>
2023-06-29 08:09:22 +0900

    fix: [wip] added s3 stack

---
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/BucketStack.java
@@ -0,0 +1,12 @@
+package jp.ijufumi.sample.vision.api.deployment.stacks;
+
+import jp.ijufumi.sample.vision.api.deployment.config.Config;
+import software.constructs.Construct;
+
+
+public class BucketStack {
+
+  public static void build(final Construct scope, final Config config) {
+
+  }
+}
