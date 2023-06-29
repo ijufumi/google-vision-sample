13c931b0856842c07bc3e97bc6234d80c33c7b2f
Takafumi Iju <ijufumi@gmail.com>
2023-06-30 07:01:57 +0900

    feat: [wip] added cloudfront stack

---
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/CloudfrontStack.java
@@ -0,0 +1,11 @@
+package jp.ijufumi.sample.vision.api.deployment.stacks;
+
+import jp.ijufumi.sample.vision.api.deployment.config.Config;
+import software.constructs.Construct;
+
+public class CloudfrontStack {
+
+  public static void build(final Construct scope, final Config config) {
+
+  }
+}
