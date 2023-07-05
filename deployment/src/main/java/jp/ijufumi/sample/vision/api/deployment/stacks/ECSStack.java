496dc0e99a8b34bc4b5e39d2b59404acaaa39398
Takafumi Iju <ijufumi@gmail.com>
2023-07-06 07:00:20 +0900

    feat: [wip] added ecs stack

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/ECSStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/ECSStack.java
@@ -1,7 +1,11 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
 import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.Compatibility;
+import software.amazon.awscdk.services.ecs.ContainerDefinitionProps;
+import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.Ec2Service;
+import software.amazon.awscdk.services.ecs.TaskDefinition;
 import software.constructs.Construct;
 
 public class ECSStack {
@@ -19,13 +23,32 @@ public class ECSStack {
         .create(scope, "app-service")
         .assignPublicIp(true)
         .cluster(ecsCluster)
+        .serviceName("app")
         .build();
+
+    var dbTaskDefinition = TaskDefinition
+        .Builder
+        .create(scope, "db-task-definition")
+        .compatibility(Compatibility.EC2)
+        .build();
+
+    var dbImage = ContainerImage
+        .fromRegistry("postgres:latest");
+
+    var dbContainer = ContainerDefinitionProps
+        .builder()
+        .containerName("db")
+        .taskDefinition(dbTaskDefinition)
+        .image(dbImage)
+        .build();
+    dbTaskDefinition.addContainer("db-container", dbContainer);
 
     Ec2Service
         .Builder
         .create(scope, "db-service")
         .assignPublicIp(false)
         .cluster(ecsCluster)
+        .serviceName("db")
         .build();
   }
 }
