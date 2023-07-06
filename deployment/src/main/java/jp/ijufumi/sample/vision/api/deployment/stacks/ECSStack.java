569af3a5d0f0f88955fffc37c9affe7e55e10fff
Takafumi Iju <ijufumi@gmail.com>
2023-07-07 08:32:29 +0900

    feat: [wip] added ecs stack

--- deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/ECSStack.java
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/ECSStack.java
@@ -1,22 +1,61 @@
 package jp.ijufumi.sample.vision.api.deployment.stacks;
 
+import java.util.List;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.Compatibility;
 import software.amazon.awscdk.services.ecs.ContainerDefinitionProps;
 import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.Ec2Service;
 import software.amazon.awscdk.services.ecs.TaskDefinition;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.constructs.Construct;
 
 public class ECSStack {
 
   public static void build(final Construct scope) {
+    // TODO: Should add authorization that needed to execute task
+    var ecsTaskRolePolicy = ManagedPolicy
+        .Builder.create(scope, "ecs-role-policy")
+        .build();
+
+    var ecsTaskServicePrincipal = ServicePrincipal
+        .Builder
+        .create("ecs-tasks.amazonaws.com")
+        .build();
+
+    var ecsRole = Role
+        .Builder
+        .create(scope, "ecs-role")
+        .roleName("ecs-role")
+        .assumedBy(ecsTaskServicePrincipal)
+        .managedPolicies(List.of(ecsTaskRolePolicy))
+        .build();
 
     var ecsCluster = Cluster
         .Builder
         .create(scope, "ecs-cluster")
         .clusterName("ecs-cluster")
         .build();
+
+    var appTaskDefinition = TaskDefinition
+        .Builder
+        .create(scope, "app-task-definition")
+        .compatibility(Compatibility.EC2)
+        .taskRole(ecsRole)
+        .build();
+
+    var appImage = ContainerImage
+        .fromRegistry("postgres:latest");
+
+    var appContainer = ContainerDefinitionProps
+        .builder()
+        .containerName("db")
+        .taskDefinition(appTaskDefinition)
+        .image(appImage)
+        .build();
+    appTaskDefinition.addContainer("app-container", appContainer);
 
     Ec2Service
         .Builder
@@ -30,6 +69,7 @@ public class ECSStack {
         .Builder
         .create(scope, "db-task-definition")
         .compatibility(Compatibility.EC2)
+        .taskRole(ecsRole)
         .build();
 
     var dbImage = ContainerImage
