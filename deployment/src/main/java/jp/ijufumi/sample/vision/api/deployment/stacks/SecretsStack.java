638f1a10c64224fe9146adbf01ede6bdae4fc369
Takafumi Iju <ijufumi@gmail.com>
2023-07-10 07:06:23 +0900

    feat: [wip] added secret manager.

---
+++ deployment/src/main/java/jp/ijufumi/sample/vision/api/deployment/stacks/SecretsStack.java
@@ -0,0 +1,10 @@
+package jp.ijufumi.sample.vision.api.deployment.stacks;
+
+import jp.ijufumi.sample.vision.api.deployment.config.Config;
+import software.constructs.Construct;
+
+public class SecretsStack {
+  public static void build(final Construct scope, final Config config) {
+
+  }
+}
