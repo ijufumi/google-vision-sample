26117e5ad4fa6dbe31dbcd92046b86068853f344
Takafumi Iju <ijufumi@gmail.com>
2023-09-18 10:42:47 +0900

    feat: added terraform for gcp

--- deployment/gcp/src/configs/config.ts
+++ deployment/gcp/src/configs/config.ts
@@ -3,9 +3,9 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-export const GCP_PROJECT = process.env.GCP_PROJECT || "" as string
+export const PROJECT = process.env.PROJECT || "" as string
 
-export const GCP_LOCATION = process.env.GCP_LOCATION || "" as string
+export const LOCATION = process.env.LOCATION || "" as string
 export const BUCKET_NAME = process.env.BACKET_NAME || "sample-bucket"
 
-export const GCP_CREDENTIALS = fs.readFileSync(process.env.CREDENTIAL_FILE || "" as string, "utf8")
+export const CREDENTIALS = fs.readFileSync(process.env.CREDENTIAL_FILE || "" as string, "utf8")
--- deployment/gcp/src/stacks/bucket.ts
+++ deployment/gcp/src/stacks/bucket.ts
@@ -1,6 +1,6 @@
 import {Construct} from "constructs";
 import * as google from "@cdktf/provider-google"
-import {GCP_LOCATION, BUCKET_NAME, GCP_PROJECT} from "../configs/config";
+import {LOCATION, BUCKET_NAME, PROJECT} from "../configs/config";
 export const createBucket = (scope: Construct) => {
   const cors = [
     {
@@ -10,8 +10,8 @@ export const createBucket = (scope: Construct) => {
     } as google.storageBucket.StorageBucketCors
   ]
   new google.storageBucket.StorageBucket(scope, 'StorageBucket', {
-    project: GCP_PROJECT,
-    location: GCP_LOCATION,
+    project: PROJECT,
+    location: LOCATION,
     name: BUCKET_NAME,
     forceDestroy: true,
     cors,
