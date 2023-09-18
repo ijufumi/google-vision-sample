f178d9dfcb2224e7a6a4fac76f2b88fef95e82a5
Takafumi Iju <ijufumi@gmail.com>
2023-09-19 06:58:41 +0900

    fix: typo

--- deployment/gcp/src/configs/config.ts
+++ deployment/gcp/src/configs/config.ts
@@ -6,6 +6,6 @@ dotenv.config()
 export const PROJECT = process.env.PROJECT || "" as string
 
 export const LOCATION = process.env.LOCATION || "" as string
-export const BUCKET_NAME = process.env.BACKET_NAME || "sample-bucket"
+export const BUCKET_NAME = process.env.BUCKET_NAME || "sample-bucket"
 
 export const CREDENTIALS = fs.readFileSync(process.env.CREDENTIAL_FILE || "" as string, "utf8")
