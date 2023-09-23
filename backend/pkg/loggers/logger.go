881efcfa00b5e2c9b8e1f7b0d15725f96189b133
Takafumi Iju <ijufumi@gmail.com>
2023-09-23 17:51:09 +0900

    wip: new logger

--- backend/pkg/loggers/logger.go
+++ backend/pkg/loggers/logger.go
@@ -5,9 +5,17 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func NewLogger() *zap.Logger {
+func NewLogger() *Logger {
+	return &Logger{buildZapLogger()}
+}
+
+func buildZapLogger() zap.Logger {
 	loggerConfig := zap.NewProductionConfig()
 	loggerConfig.Level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
-	logger, _ := loggerConfig.Build()
-	return logger
+	l, _ := loggerConfig.Build()
+	return *l
+}
+
+type Logger struct {
+	zap.Logger
 }
