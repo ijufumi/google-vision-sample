16348c4d1442878e51de3e2e066efe7e6108d762
Takafumi Iju <ijufumi@gmail.com>
2023-10-21 14:34:52 +0900

    wip: restructured

--- backend/internal/usecases/repositories/extracted_text_repository.go
+++ backend/internal/usecases/repositories/extracted_text_repository.go
@@ -1,12 +1,12 @@
 package repositories
 
 import (
-	"github.com/ijufumi/google-vision-sample/internal/models/entities"
+	models "github.com/ijufumi/google-vision-sample/internal/models/entities"
 	"gorm.io/gorm"
 )
 
 type ExtractedTextRepository interface {
-	GetByID(db *gorm.DB, id string) (*entities.ExtractedText, error)
-	Create(db *gorm.DB, entity ...*entities.ExtractedText) error
+	GetByID(db *gorm.DB, id string) (*models.ExtractedText, error)
+	Create(db *gorm.DB, entity ...*models.ExtractedText) error
 	DeleteByOutputFileID(db *gorm.DB, outputFileID string) error
 }
