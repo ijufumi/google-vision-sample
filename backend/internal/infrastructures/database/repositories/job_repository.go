d222dc72f683e70a4901553c5c78dd85de36edfe
Takafumi Iju <ijufumi@gmail.com>
2023-11-09 07:03:10 +0900

    wip: restructured

--- backend/internal/infrastructures/database/repositories/job_repository.go
+++ backend/internal/infrastructures/database/repositories/job_repository.go
@@ -5,6 +5,7 @@ import (
 	models "github.com/ijufumi/google-vision-sample/internal/models/entities"
 	repositoryInterface "github.com/ijufumi/google-vision-sample/internal/usecases/repositories"
 	"github.com/pkg/errors"
+	"golang.org/x/net/context"
 	"gorm.io/gorm"
 )
 
@@ -13,47 +14,66 @@ func NewJobRepository() repositoryInterface.JobRepository {
 }
 
 type jobRepository struct {
+	baseRepository
 }
 
-func (r *jobRepository) GetAll(db *gorm.DB) ([]*models.Job, error) {
+func (r *jobRepository) GetAll(ctx context.Context) ([]*models.Job, error) {
 	var jobs *entities.Jobs
-	if err := db.
-		Find(&jobs).Error; err != nil {
-		return nil, errors.Wrap(err, "JobRepository#GetAll")
+	err := r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.
+			Find(&jobs).Error; err != nil {
+			return errors.Wrap(err, "JobRepository#GetAll")
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 	return jobs.ToModel(), nil
 }
 
-func (r *jobRepository) GetByID(db *gorm.DB, id string) (*models.Job, error) {
+func (r *jobRepository) GetByID(ctx context.Context, id string) (*models.Job, error) {
 	var job entities.Job
-	if err := db.
-		Preload("InputFiles").
-		Preload("InputFiles.OutputFiles.ExtractedTexts").
-		Where("id = ?", id).First(&job).Error; err != nil {
-		return nil, errors.Wrap(err, "JobRepository#GetByID")
+	err := r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.
+			Preload("InputFiles").
+			Preload("InputFiles.OutputFiles.ExtractedTexts").
+			Where("id = ?", id).First(&job).Error; err != nil {
+			return errors.Wrap(err, "JobRepository#GetByID")
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 	return job.ToModel(), nil
 }
 
-func (r *jobRepository) Create(db *gorm.DB, model *models.Job) error {
+func (r *jobRepository) Create(ctx context.Context, model *models.Job) error {
 	entity := entities.FromJobModel(model)
-	if err := db.Create(entity).Error; err != nil {
-		return errors.Wrap(err, "JobRepository#Create")
-	}
-	return nil
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Create(entity).Error; err != nil {
+			return errors.Wrap(err, "JobRepository#Create")
+		}
+		return nil
+	})
 }
 
-func (r *jobRepository) Update(db *gorm.DB, model *models.Job) error {
+func (r *jobRepository) Update(ctx context.Context, model *models.Job) error {
 	entity := entities.FromJobModel(model)
-	if err := db.Save(entity).Error; err != nil {
-		return errors.Wrap(err, "JobRepository#Update")
-	}
-	return nil
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Save(entity).Error; err != nil {
+			return errors.Wrap(err, "JobRepository#Update")
+		}
+		return nil
+	})
 }
 
-func (r *jobRepository) Delete(db *gorm.DB, id string) error {
-	if err := db.Where("id = ?", id).Delete(&entities.Job{}).Error; err != nil {
-		return errors.Wrap(err, "JobRepository#Delete")
-	}
-	return nil
+func (r *jobRepository) Delete(ctx context.Context, id string) error {
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Where("id = ?", id).Delete(&entities.Job{}).Error; err != nil {
+			return errors.Wrap(err, "JobRepository#Delete")
+		}
+		return nil
+	})
 }
