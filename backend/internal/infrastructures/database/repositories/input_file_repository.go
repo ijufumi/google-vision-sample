b7013a891c1c9f13d77b1f5f8f4e8c6fdd5e5ac8
Takafumi Iju <ijufumi@gmail.com>
2023-11-12 08:47:28 +0900

    wip: restructured

--- backend/internal/infrastructures/database/repositories/input_file_repository.go
+++ backend/internal/infrastructures/database/repositories/input_file_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"context"
 	"github.com/ijufumi/google-vision-sample/internal/infrastructures/database/entities"
 	models "github.com/ijufumi/google-vision-sample/internal/models/entities"
 	repositoryInterface "github.com/ijufumi/google-vision-sample/internal/usecases/repositories"
@@ -13,41 +14,63 @@ func NewInputFileRepository() repositoryInterface.InputFileRepository {
 }
 
 type inputFileRepository struct {
+	baseRepository
 }
 
-func (r *inputFileRepository) GetByID(db *gorm.DB, id string) (*models.InputFile, error) {
+func (r *inputFileRepository) GetByID(ctx context.Context, id string) (*models.InputFile, error) {
 	var result entities.InputFile
-	if err := db.First(&result, "id = ?", id).Error; err != nil {
-		return nil, errors.Wrap(err, "InputFileRepository#GetByID")
+	err := r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.First(&result, "id = ?", id).Error; err != nil {
+			return errors.Wrap(err, "InputFileRepository#GetByID")
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
+
 	return result.ToModel(), nil
 }
 
-func (r *inputFileRepository) GetByJobID(db *gorm.DB, jobID string) ([]*models.InputFile, error) {
+func (r *inputFileRepository) GetByJobID(ctx context.Context, jobID string) ([]*models.InputFile, error) {
 	var results entities.InputFiles
-	if err := db.Where("job_id = ?", jobID).Find(&results).Error; err != nil {
-		return nil, errors.Wrap(err, "InputFileRepository#GetByJobID")
+	err := r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Where("job_id = ?", jobID).Find(&results).Error; err != nil {
+			return errors.Wrap(err, "InputFileRepository#GetByJobID")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
+
 	return results.ToModel(), nil
 }
 
-func (r *inputFileRepository) Create(db *gorm.DB, entity ...*models.InputFile) error {
-	if err := db.Create(&entity).Error; err != nil {
-		return errors.Wrap(err, "InputFileRepository#Create")
-	}
-	return nil
+func (r *inputFileRepository) Create(ctx context.Context, entity ...*models.InputFile) error {
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Create(&entity).Error; err != nil {
+			return errors.Wrap(err, "InputFileRepository#Create")
+		}
+		return nil
+	})
 }
 
-func (r *inputFileRepository) Update(db *gorm.DB, entity *models.InputFile) error {
-	if err := db.Save(&entity).Error; err != nil {
-		return errors.Wrap(err, "InputFileRepository#Update")
-	}
-	return nil
+func (r *inputFileRepository) Update(ctx context.Context, entity *models.InputFile) error {
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Save(&entity).Error; err != nil {
+			return errors.Wrap(err, "InputFileRepository#Update")
+		}
+		return nil
+	})
 }
 
-func (r *inputFileRepository) DeleteByJobID(db *gorm.DB, jobID string) error {
-	if err := db.Where("job_id = ?", jobID).Delete(&entities.InputFile{}).Error; err != nil {
-		return errors.Wrap(err, "InputFileRepository#DeleteByJobID")
-	}
-	return nil
+func (r *inputFileRepository) DeleteByJobID(ctx context.Context, jobID string) error {
+	return r.Transaction(ctx, func(tx *gorm.DB) error {
+		if err := tx.Where("job_id = ?", jobID).Delete(&entities.InputFile{}).Error; err != nil {
+			return errors.Wrap(err, "InputFileRepository#DeleteByJobID")
+		}
+		return nil
+	})
 }
