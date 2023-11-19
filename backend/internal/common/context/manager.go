9f02d15ec67a0e354922b1f940902035105be650
Takafumi Iju <ijufumi@gmail.com>
2023-11-19 09:38:28 +0900

    wip: restructured

--- backend/internal/common/context/manager.go
+++ backend/internal/common/context/manager.go
@@ -7,68 +7,72 @@ import (
 	"gorm.io/gorm"
 )
 
+const contextKey_DB = "db"
+const contextKey_Tx = "tx"
+const contextKey_Logger = "logger"
+
 func SetDBToGinContext(ctx *gin.Context, db *gorm.DB) {
-	ctx.Set("db", db)
+	ctx.Set(contextKey_DB, db)
 }
 
 func SetLoggerToGinContext(ctx *gin.Context, logger *zap.Logger) {
-	ctx.Set("logger", logger)
+	ctx.Set(contextKey_Logger, logger)
 }
 
 func GetLoggerFromGinContext(ctx *gin.Context) *zap.Logger {
-	l, _ := ctx.Get("logger")
+	l, _ := ctx.Get(contextKey_Logger)
 	return l.(*zap.Logger)
 }
 
 func GetDBFromGinContext(ctx *gin.Context) *gorm.DB {
-	d, _ := ctx.Get("db")
+	d, _ := ctx.Get(contextKey_DB)
 	return d.(*gorm.DB)
 }
 
 func GetContext(ctx *gin.Context) context.Context {
 	l := GetLoggerFromGinContext(ctx)
 	d := GetDBFromGinContext(ctx)
-	_ctx := context.WithValue(ctx.Request.Context(), "logger", l)
-	return context.WithValue(_ctx, "db", d)
+	_ctx := context.WithValue(ctx.Request.Context(), contextKey_Logger, l)
+	return context.WithValue(_ctx, contextKey_DB, d)
 }
 
 func GetLogger(ctx context.Context) *zap.Logger {
-	l := ctx.Value("logger")
+	l := ctx.Value(contextKey_Logger)
 	return l.(*zap.Logger)
 }
 
 func SetLogger(ctx context.Context, logger *zap.Logger) context.Context {
-	return context.WithValue(ctx, "logger", logger)
+	return context.WithValue(ctx, contextKey_Logger, logger)
 }
 
 func GetDB(ctx context.Context) *gorm.DB {
-	d := ctx.Value("tx")
+	d := ctx.Value(contextKey_Tx)
 	if d != nil {
 		return d.(*gorm.DB)
 	}
-	d = ctx.Value("db")
+	d = ctx.Value(contextKey_DB)
 	return d.(*gorm.DB)
 }
 
 func SetDB(ctx context.Context, db *gorm.DB) context.Context {
-	return context.WithValue(ctx, "db", db)
+	return context.WithValue(ctx, contextKey_DB, db)
 }
 
 func SetTx(ctx context.Context, tx *gorm.DB) context.Context {
-	return context.WithValue(ctx, "tx", tx)
+	return context.WithValue(ctx, contextKey_Tx, tx)
 }
 
 func NewContext(ctx context.Context) context.Context {
 	ctx2 := context.Background()
-	tx := ctx.Value("tx")
+	tx := ctx.Value(contextKey_Tx)
 	if tx != nil {
 		ctx2 = SetTx(ctx2, tx.(*gorm.DB))
 	}
-	db := ctx.Value("db")
+	db := ctx.Value(contextKey_DB)
 	if db != nil {
 		ctx2 = SetDB(ctx2, db.(*gorm.DB))
 	}
-	l := ctx.Value("logger")
+	l := ctx.Value(contextKey_Logger)
 	if l != nil {
 		ctx2 = SetLogger(ctx2, l.(*zap.Logger))
 	}
