146955a27dac725350b05fe5396c3a5a13fed55d
Takafumi Iju <ijufumi@gmail.com>
2023-11-23 09:41:31 +0900

    wip: restructured

--- backend/internal/common/context/manager.go
+++ backend/internal/common/context/manager.go
@@ -37,22 +37,24 @@ func GetContext(ctx *gin.Context) context.Context {
 }
 
 func GetLogger(ctx context.Context) *zap.Logger {
-	l := ctx.Value(contextKey_Logger)
-	return l.(*zap.Logger)
+	return getValue[*zap.Logger](ctx, contextKey_Logger)
+}
+
+func GetDB(ctx context.Context) *gorm.DB {
+	return getValue[*gorm.DB](ctx, contextKey_DB)
+}
+
+func GetTx(ctx context.Context) *gorm.DB {
+	return getValue[*gorm.DB](ctx, contextKey_Tx)
+}
+
+func getValue[T any](ctx context.Context, contextKey string) T {
+	v := ctx.Value(contextKey)
+	return v.(T)
 }
 
 func SetLogger(ctx context.Context, logger *zap.Logger) context.Context {
 	return context.WithValue(ctx, contextKey_Logger, logger)
-}
-
-func GetDB(ctx context.Context) *gorm.DB {
-	d := ctx.Value(contextKey_DB)
-	return d.(*gorm.DB)
-}
-
-func GetTx(ctx context.Context) *gorm.DB {
-	d := ctx.Value(contextKey_Tx)
-	return d.(*gorm.DB)
 }
 
 func SetDB(ctx context.Context, db *gorm.DB) context.Context {
